83ffd7c64ce4a22a90165e218d909e2e6930af2f
lihongsai <627990606@qq.com>
2021-02-25 10:27:31 +0800

    Initial commit

---
+++ src/main/java/com/heb/guitar/mapper/SysUserMapper.java
@@ -0,0 +1,31 @@
+package com.heb.guitar.mapper;
+
+import com.heb.guitar.entity.SysUser;
+import com.heb.guitar.vo.req.UserReqVO;
+import java.util.List;
+
+public interface SysUserMapper {
+    int deleteByPrimaryKey(String id);
+
+    int insert(SysUser record);
+
+    int insertSelective(SysUser record);
+
+    SysUser selectByPrimaryKey(String id);
+
+    int updateByPrimaryKeySelective(SysUser record);
+
+    int updateByPrimaryKey(SysUser record);
+
+    SysUser getUserInfoByName(String username);
+
+    List<SysUser> selectAll(UserReqVO vo);
+
+    long userCount(UserReqVO vo);
+
+    //根据部门id集合查找用户
+    List<SysUser> selectUserInfoByDeptIds (List<String> deptIds);
+
+    SysUser getUserByUsername(String username);
+
+}
